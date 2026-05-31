5adfa44f4181ba97dbfd5b7d939a4431e613a019
Steimber <randyarteaga1519@gmail.com>
2026-05-30 19:13:42 -0500

    cambiois

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,4 +1,4 @@
-// Validación de formulario de Login
+
 const loginForm = document.getElementById('loginForm');
 if (loginForm) {
     loginForm.addEventListener('submit', function(e) {
@@ -10,12 +10,12 @@ if (loginForm) {
     });
 }
 
-// Validación de formulario de Registro
+
 const registerForm = document.getElementById('registerForm');
 if (registerForm) {
     registerForm.addEventListener('submit', function(e) {
         e.preventDefault();
-        // Redirigir después de 2 segundos
+       
         setTimeout(() => {
             window.location.href = 'index.html';
         }, 2000);
